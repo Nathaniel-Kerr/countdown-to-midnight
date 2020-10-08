5f7f7f760b9e37679198806f26484aecd408b02b
Nathaniel-Kerr <NathanielKerr10@gmail.com>
2020-10-07 23:32:49 -0700

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
-#write your code here
+midnight_in = 10
 
-def countdown
+def countdown_with_sleep(midnight_in)
+    sleep 5 
+ end
+
+def countdown(midnight_in)
+    while midnight_in > 0
+        puts "#{midnight_in} SECOND(S)!"
+        midnight_in -=1
+    end
+    return "HAPPY NEW YEAR!"
 end
